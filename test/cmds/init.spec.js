762ec372fbcad36b091588e9e3cbc781aae598fa
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-04-17 16:16:48 -0700

    feat: added `gally init` + test

---
+++ test/cmds/init.spec.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const expect = require('chai').expect;
+const tmp = require('tmp');
+const init = require("./../../src/cmds/init");
+
+tmp.setGracefulCleanup();
+
+describe("Testing `init`", () => {
+  it("Testing basic file copying", (done) => {
+    const dir = tmp.dirSync({ keep: false, unsafeCleanup: true }).name;
+    const cwd = process.cwd();
+    process.chdir(dir);
+    init.handler({}).then(() => {
+      expect(fs.existsSync(path.join(dir, ",gally.json"))).to.equal(true);
+      process.chdir(cwd);
+      done();
+    });
+  });
+});
