8148e038ca1a781b339335afc35a11df18408e43
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-04-19 10:55:36 -0700

    chore: added sync test

---
+++ test/cmds/sync.spec.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const sync = require("./../../src/cmds/sync");
+const gally = require("./../../src/gally");
+const github = require("./../../src/util/github");
+
+describe("Testing `sync <remote>`", () => {
+  let gallyLoad;
+  let githubEvaludate;
+
+  before(() => {
+    gallyLoad = gally.load;
+    gally.load = () => Promise.resolve({});
+    githubEvaludate = github.evaluate;
+    github.evaluate = () => Promise.resolve({});
+  });
+
+  after(() => {
+    github.evaluate = githubEvaludate;
+    gally.load = gallyLoad;
+  });
+
+  it("Testing sync (Integration)", (done) => {
+    sync.handler({}).then((r) => {
+      expect(r).to.deep.equal({});
+      done();
+    });
+  });
+});
