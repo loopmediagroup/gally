3e1a01b6f07b1410f11d3f29fffa742e1d86d90b
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-04-18 17:19:55 -0700

    feat: added sync command

---
+++ src/cmds/sync.js
@@ -0,0 +1,10 @@
+const os = require("os");
+const gally = require("./../gally");
+const github = require("./../util/github");
+
+exports.command = 'sync <remote>';
+exports.desc = 'Apply local configuration to remote github repo.';
+exports.builder = {};
+exports.handler = argv => gally
+  .load(`${os.homedir()}/.gally`, process.cwd())
+  .then(config => github.evaluate(config, argv.remote));
