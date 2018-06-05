a9fcabae6d56eaa7942852da29245a6133ee6f9f
Lukas Siemon <lukas.siemon@getintheloop.ca>
2018-06-05 13:30:53 -0700

    fix: stubbed gally.load correctly

--- test/cmds/promote.spec.js
+++ test/cmds/promote.spec.js
@@ -1,14 +1,18 @@
 const expect = require('chai').expect;
 const open = require("./../../src/util/open");
 const promote = require("./../../src/cmds/promote");
+const gally = require("./../../src/gally");
 const git = require("./../../src/util/git");
 
 describe("Testing `promote <branch>`", () => {
+  let gallyLoad;
   const urls = [];
   let gitGhPromoteUrl;
   let openUrl;
 
   before(() => {
+    gallyLoad = gally.load;
+    gally.load = () => Promise.resolve({});
     gitGhPromoteUrl = git.ghPromoteUrl;
     git.ghPromoteUrl = () => Promise.resolve("URL");
     openUrl = open.url;
@@ -20,6 +24,7 @@ describe("Testing `promote <branch>`", () => {
   after(() => {
     git.ghPromoteUrl = gitGhPromoteUrl;
     open.url = openUrl;
+    gally.load = gallyLoad;
   });
 
   beforeEach(() => {
